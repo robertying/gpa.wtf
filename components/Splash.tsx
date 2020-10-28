bd387053246eb715be428f89ad2264e7b24c3c2b
Rui Ying <yingrui205@gmail.com>
2020-10-28 17:58:03 +0800

    Upgrade dependencies

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -1,9 +1,10 @@
 import { CircularProgress } from "@material-ui/core";
+import Image from "next/image";
 
 const Splash: React.FC = () => {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center space-y-4">
-      <img className="w-24 h-24" src="/logo.png" alt="Logo" />
+      <Image className="w-24 h-24" src="/logo.png" alt="Logo" unsized />
       <CircularProgress size="1.5rem" />
     </div>
   );
